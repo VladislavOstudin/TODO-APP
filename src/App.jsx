4a6965aecf3fd6f4664668898fdcbeb73d7de830
Vlad Ostudin <ostudin.v@gmail.com>
2025-03-06 23:13:22 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>My TODO App</h1>
+
+      </main> 
+    </>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+export default function Header() {
+  const [now, setNow] = useState(new Date())
+
+  setInterval(() => setNow(new Date()) ,1000)
+
+    return (
+      <header>
+      <span>Time now: { now.toLocaleTimeString() }</span>
+    </header>
+    )
+  }
